7cb72bb4e0c7b07d306cc2a28d96ac2882de075a
santosgo <goncalo.a.santos@gmail.com>
2022-05-15 19:29:09 +0200

    fixed entry_points in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HOME_DIR / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pymailorganizer",
-    version="0.5.4",
+    version="0.5.6",
     description="Python command line mail organizer",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -41,7 +41,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "pymailorganizer=pymailorganizer:main",
+            "pymailorganizer = pymailorganizer.main:shell",
         ]
     },
 )
